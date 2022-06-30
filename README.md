de8e422a46b0b83f90aba1c5953327285495fe65
WonhoZhung <zpeter97@kaist.ac.kr>
2022-06-30 15:19:29 +0900

    major update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # sdtether_practice
 
 tethered docking practice using RxDock
+
+## Example
+```
+python main.py -p example/5r7y_pocket_ref.pdb -l example/5r7y_ligand_0.sdf -r example/5r7y_ligand_ref.sdf -s "CS(=O)(=O)NCCC=1C=CC=CC1" -n 10 -o 5r7y
+```
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import os
 
 
-AVAIL_IN_FORMAT_LIST = ["sdf", "pdb", "mol2", "sd", "xyz"]
+AVAIL_IN_FORMAT_LIST = ["sdf", "pdb", "mol2", "xyz"]
 
 
 def generate_prm(
         ref_poc_fn,
         ref_lig_fn,
         out_prm_fn,
-        title=""
+        title="",
+        max_trans=1.0,
+        max_rot=10.0,
+        max_dihedral=10.0
         ):
     assert ref_poc_fn.split('.')[-1] == "mol2", "Wrong receptor format" 
-    assert ref_lig_fn.split('.')[-1] == "sd", "Wrong ligand format"
+    assert ref_lig_fn.split('.')[-1] == "sdf", "Wrong ligand format"
     assert out_prm_fn.split('.')[-1] == "prm", "Wrong param format"
     
     lines = f"""RBT_PARAMETER_FILE_V1.00
@@ -23,9 +26,9 @@ SECTION LIGAND
     TRANS_MODE TETHERED
     ROT_MODE TETHERED 
     DIHEDRAL_MODE TETHERED 
-    MAX_TRANS 1.0 
-    MAX_ROT 10.0 
-    MAX_DIHEDRAL 10.0
+    MAX_TRANS {max_trans} 
+    MAX_ROT {max_rot}
+    MAX_DIHEDRAL {max_dihedral}
 END_SECTION
 
 ##################################################################
@@ -56,39 +59,51 @@ END_SECTION"""
 
 def prepare_pocket(
         in_poc_fn,
-        out_poc_fn # should be .mol2 format
+        out_poc_fn, # should be .mol2 format
+        verbose=False
         ):
     if os.path.exists(out_poc_fn):
         return 0
     assert in_poc_fn.split('.')[-1] in AVAIL_IN_FORMAT_LIST, "Wrong input format" 
     assert out_poc_fn.split('.')[-1] == "mol2", "Wrong output format"
-    command = f"obabel {in_poc_fn} -O {out_poc_fn}"
-    print(command)
+    command = f"obabel {in_poc_fn} -O {out_poc_fn} "
+    if not verbose:
+        command += ">& /dev/null"
+    else:
+        print(command)
     tag = os.system(command)
     return tag
 
 def prepare_ligand(
         ref_lig_fn,
         in_lig_fn,
-        out_lig_fn, # should be .sd format
-        smarts
+        out_lig_fn, 
+        smarts,
+        verbose=False
         ):
     if os.path.exists(out_lig_fn):
         return 0
     assert in_lig_fn.split('.')[-1] in AVAIL_IN_FORMAT_LIST, "Wrong input format" 
     assert ref_lig_fn.split('.')[-1] in AVAIL_IN_FORMAT_LIST, "Wrong ref format" 
-    assert out_lig_fn.split('.')[-1] == "sd", "Wrong output format"
-    command = f"sdtether {ref_lig_fn} {in_lig_fn} {out_lig_fn} '{smarts}'"
-    print(command)
+    assert out_lig_fn.split('.')[-1] == "sdf", "Wrong output format"
+    command = f"sdtether {ref_lig_fn} {in_lig_fn} {out_lig_fn} '{smarts}' "
+    if not verbose:
+        command += ">& /dev/null"
+    else:
+        print(command)
     tag = os.system(command)
     return tag
 
 def make_grid(
-        in_prm_fn
+        in_prm_fn,
+        verbose=False
         ):
     assert in_prm_fn.split('.')[-1] == "prm", "Wrong param format"
-    command = f"rbcavity -W -d -r {in_prm_fn}"
-    print(command)
+    command = f"rbcavity -W -d -r {in_prm_fn} "
+    if not verbose:
+        command += ">& /dev/null"
+    else:
+        print(command)
     tag = os.system(command)
     return tag
 
@@ -96,13 +111,19 @@ def run_docking(
         in_lig_fn,
         out_lig_prefix,
         in_prm_fn,
-        n=1,
-        seed=0,
+        n,
+        seed=None,
+        verbose=False
         ):
-    assert in_lig_fn.split('.')[-1] == "sd", "Wrong input format"
+    assert in_lig_fn.split('.')[-1] == "sdf", "Wrong input format"
     assert in_prm_fn.split('.')[-1] == "prm", "Wrong param format"
-    command = f"rbdock -i {in_lig_fn} -o {out_lig_prefix} -r {in_prm_fn} -p dock.prm -n {n} --seed {seed}"
-    print(command)
+    command = f"rbdock -i {in_lig_fn} -o {out_lig_prefix} -r {in_prm_fn} -p dock.prm -n {n} "
+    if seed is not None:
+        command += "--seed {seed} "
+    if not verbose:
+        command += ">& /dev/null"
+    else:
+        print(command)
     tag = os.system(command)
     return tag
 
@@ -110,45 +131,49 @@ def run_docking(
 if __name__ == "__main__":
 
     import argparse
+    import tempfile
     from time import time
     
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-p", help="protein_fn in PDB format", type=str)
-    parser.add_argument("-l", help="ligand_fn in SDF format", type=str)
-    parser.add_argument("-c", help="complex_fn in PDB format", type=str)
-    parser.add_argument("-t", help="traj_fn in DCD format", type=str)
-    parser.add_argument("-o", help="log_fn", type=str)
+    parser.add_argument("-p", help="protein in PDB format", type=str, required=True)
+    parser.add_argument("-l", help="input ligand in SDF format", type=str, required=True)
+    parser.add_argument("-r", help="reference ligand in SDF format", type=str, required=True)
+    parser.add_argument("-s", help="SMILES to be tethered", type=str, required=True)
+    parser.add_argument("-o", help="output ligand prefix", type=str, required=True)
+    parser.add_argument("-n", help="num docking per ligand", type=int, default=1)
+    parser.add_argument("--seed", help="seed", type=int, default=None)
     parser.add_argument("-v", help="verbose", action="store_true")
 
     args = parser.parse_args()
 
-    KEY = "1jzs"
-    N = 1
-    lig_ori_fn = f"files/{KEY}_ligand_0.sdf"
-    lig_ref_fn = f"files/{KEY}_ligand.sd"
-    poc_ori_fn = f"files/{KEY}_protein_cleaned.pdb"
-    scaff_smi = "C1COCC(CC2CO2)C1"
+    fd, path = tempfile.mkstemp(suffix=".prm", prefix="SD_tmp_", dir="/tmp")
 
-    lig_in_fn = f"files/{KEY}_ligand_input.sd"
-    lig_out_prefix = f"files/{KEY}_ligand_dock"
-    poc_in_fn = f"files/{KEY}_protein_cleaned.mol2"
+    try: 
+        st = time()
+        prepare_ligand(args.r, args.l, args.l, args.s, verbose=args.v)
+        prepare_pocket(args.p, args.p[:-3]+"mol2", verbose=args.v)
+        if args.v:
+            print(f"Prepare inputs done --> Duration: {time() - st:.2f}(s)")
 
-    prm_fn = f"files/{KEY}.prm"
+        st = time()
+        generate_prm(args.p[:-3]+"mol2", args.r, path, args.s)
+        if args.v:
+            print(f"Generate param file done --> Duration: {time() - st:.2f}(s)")
 
-    st = time()
-    prepare_ligand(lig_ref_fn, lig_ori_fn, lig_in_fn, scaff_smi)
-    prepare_pocket(poc_ori_fn, poc_in_fn)
-    print(f"{time() - st:.2f}(s)")
+        st = time()
+        make_grid(path, verbose=args.v)
+        if args.v:
+            print(f"Making grid done --> Duration: {time() - st:.2f}(s)")
 
-    generate_prm(poc_in_fn, lig_ref_fn, prm_fn, "test")
-    print(f"{time() - st:.2f}(s)")
+        st = time()
+        run_docking(args.l, args.o, path, n=args.n, seed=args.seed, verbose=args.v)
+        if args.v:
+            print(f"Running docking done --> Duration: {time() - st:.2f}(s)")
+    except Exception as e:
+        print(e)
 
-    make_grid(prm_fn)
-    print(f"{time() - st:.2f}(s)")
-
-    run_docking(lig_in_fn, lig_out_prefix, prm_fn, n=N)
-    print(f"{time() - st:.2f}(s)")
-
+    os.close(fd)
+    os.unlink(path)
 
 
